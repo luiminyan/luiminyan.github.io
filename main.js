4d20992891fdb727b567fef5af430cef400d475b
Minh <luiminyan@gmail.com>
2023-12-10 13:44:27 +0100

    append git link into button in project dic:   index.html 	added comments for back to top function:   main.js 	changed left and right cols orders:   style.css

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-// set onscroll function of the window
+// set onscroll function of the current window
 window.onscroll = function() {
     scrollFunction()
 };
  
 function scrollFunction() {
-    // scrolled for more than 40px, show the scroll back to top button
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        // scrolled for more than 40px, show the scroll back to top button
         document.getElementById("back-to-top-btn").style.display = "block";
     } else {
+        // otherwise: hide button
         document.getElementById("back-to-top-btn").style.display = "none";
     }
 }
  
 // click on the button and scroll back
 function topFunction() {
+    // by reset the scrollTop value
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
